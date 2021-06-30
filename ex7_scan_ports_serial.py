a9148797640b9fc1de9eab51580e018517dbfaac
Adam Johnson <me@adamj.eu>
2021-06-30 17:30:16 +0100

    Commit next exercises

--- ex7_scan_ports_serial.py
+++ ex7_scan_ports_serial.py
@@ -1,24 +1,29 @@
 from __future__ import annotations
 
 import argparse
+import time
 from socket import AF_INET, SOCK_STREAM, socket
 
 
 def main(args=None) -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument("host")
-    parser.add_argument("port", type=int)
+    parser.add_argument("min_port", type=int)
+    parser.add_argument("max_port", type=int)
     parser.add_argument("--timeout", type=float, default=1.0)
     args = parser.parse_args(args)
+    if args.max_port < args.min_port:
+        parser.error("max_port should be greater than or equal to min_port")
 
-    is_open = check_port(args.host, args.port, args.timeout)
+    start = time.perf_counter_ns()
 
-    if is_open:
-        print(f"Port {args.port} is open")
-        return 0
-    else:
-        print(f"Port {args.port} is not open")
-        return 1
+    for port in range(args.min_port, args.max_port + 1):
+        if check_port(args.host, port, args.timeout):
+            print(f"Port {port} is open")
+
+    duration = (time.perf_counter_ns() - start) / 1e9
+    print(f"Completed scan in {duration} seconds")
+    return 0
 
 
 def check_port(host: str, port: int, timeout: float) -> bool:
---
+++ ex9_count_up_threads.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+import argparse
+import time
+from threading import Thread
+
+
+COUNTER = 0
+
+
+def main(args=None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("target", type=int)
+    args = parser.parse_args(args)
+
+    threads = [Thread(target=increment) for _ in range(args.target)]
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    print(f"Counted up to {COUNTER}")
+
+    return 0
+
+
+def increment() -> None:
+    global COUNTER
+    current = COUNTER
+    time.sleep(0.0001)  # Simulate long calculation or I/O
+    COUNTER = current + 1
+
+
+if __name__ == "__main__":
+    exit(main())
